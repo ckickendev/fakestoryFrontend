8ede5756230890800c4544b8a6d5a047a77886ae
Gin <thesoonafu@gmail.com>
2022-03-01 22:58:53 +0700

    change tree

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./css/App.css";
 import { Routes, Route } from "react-router-dom";
 import Profile from "./nav/Profile";
 import Main from "./nav/Main";
+import Login from "./components/login/Login";
 
 function App() {
   /* const [{ user }, dispatch] = useStateValue(); */
 
+  const user = undefined;
   return (
     <div className="app">
-      {/* {!user ? (
+
+      { !user ? (
         <Login />
-      ) : ( */}
+      ) : (
       <>
         <Header />
-
         <div className="app__body">
           <Routes>
             <Route path="/" element={<Main />} />
@@ -23,7 +25,8 @@ function App() {
           </Routes>
         </div>
       </>
-      {/* )} */}
+      )}
+      
     </div>
   );
 }
