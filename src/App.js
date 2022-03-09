ebb6e0125aa2778f544cb23fd26d9a95fb5b2bd2
Gin <thesoonafu@gmail.com>
2022-03-09 13:01:54 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -1,59 +1,47 @@
 import React from "react";
-<<<<<<< HEAD
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import Feed from "./Feed";
-import Advertisement from "./Advertisement";
-import Login from "./Login";
+import Header from "../src/components/main/Header";
 import "./css/App.css";
-=======
+import "./css/App.css";
 import Header from "../src/components/main/Header";
 import "./css/App.css";
 import { Routes, Route } from "react-router-dom";
-import Profile from "./nav/Profile";
-import Main from "./nav/Main";
 import Login from "./components/login/Login";
->>>>>>> e515c125dd72fb8fa407c7ffab3ac2dd4e603541
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import authReducer from "./store/reducers/auth";
+import Register from "./components/register/Register"
+import checkLogin from "./components/login/LogicLogin";
+
+
+const rootReducer = combineReducers({
+  auth : authReducer
+});
+
+const store = createStore(rootReducer);
 
 function App() {
-  /* const [{ user }, dispatch] = useStateValue(); */
-
-<<<<<<< HEAD
+  const isLogin = checkLogin();
   return (
-    <div className="app">
-      {/* {!user ? (
-        <Login />
-      ) : ( */}
-      <>
-        <Header />
+    <Provider store={store}>
+      <div className="app">
+        {!isLogin ? (  
+          <Login />
+        ) : (
+          <>
+            {/* <Header /> */}
+            <div className="app__body">
+              <Routes>
+                <Route path="/login" element={<Login/>} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Main />} />
+              </Routes>
+            </div>
+          </>
+        )}
 
-        <div className="app__body">
-          <Sidebar />
-          <Feed />
-          <Advertisement />
-        </div>
-      </>
-      {/* )} */}
-=======
-  const user = "minh huy";
-  return (
-    <div className="app">
-      {!user ? (
-        <Login />
-      ) : (
-        <>
-          <Header />
-          <div className="app__body">
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </div>
-        </>
-      )}
->>>>>>> e515c125dd72fb8fa407c7ffab3ac2dd4e603541
-    </div>
-  );
-}
-
+      </div>
+    </Provider>
+  )
+        }
 export default App;
