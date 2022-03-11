bf76afa2e9f3599926ef145b90c889490ac95fbc
Thinh Hoang <hoangthinhpro2001@gmail.com>
2022-03-11 10:44:09 +0700

    Fix Profile Page 11/03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import "./css/App.css";
-<<<<<<< HEAD
-=======
-import Header from "../src/components/main/Header";
->>>>>>> 9220d62f8915345240a956ff4bb5b0e242c5110d
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import { createStore, combineReducers } from "redux";
@@ -12,32 +8,19 @@ import Main from "./nav/Main";
 import authReducer from "./store/reducers/auth";
 import inforReducer from "./store/reducers/information";
 import checkLogin from "./components/login/LogicLogin";
-<<<<<<< HEAD
 import Profile from "./nav/Profile";
 import Register from "./components/register/Register";
-import UploadDemo from "./firebase/UploadDemo"
-
-
-const rootReducer = combineReducers({
-  auth : authReducer,
-  information : inforReducer
-=======
-import Register from "./Register";
-import Profile from "./nav/Profile";
-import Main from "./nav/Main";
+import UploadDemo from "./firebase/UploadDemo";
 
 const rootReducer = combineReducers({
   auth: authReducer,
->>>>>>> 9220d62f8915345240a956ff4bb5b0e242c5110d
+  information: inforReducer,
 });
 
 const store = createStore(rootReducer);
 
 function App() {
-<<<<<<< HEAD
   // const isLogin = checkLogin();
-=======
->>>>>>> 9220d62f8915345240a956ff4bb5b0e242c5110d
   const isLogin = true;
   return (
     <Provider store={store}>
@@ -46,17 +29,10 @@ function App() {
           <Login />
         ) : (
           <>
-<<<<<<< HEAD
             <div className="app__body">
               <Routes>
-                <Route path="/firebase" element= {<UploadDemo />} />
-                <Route path="/login" element={<Login/>} />
-=======
-            <Header />
-            <div className="app__body">
-              <Routes>
+                <Route path="/firebase" element={<UploadDemo />} />
                 <Route path="/login" element={<Login />} />
->>>>>>> 9220d62f8915345240a956ff4bb5b0e242c5110d
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/" element={<Main />} />
--- src/components/main/Advertisement.js
+++ src/components/main/Advertisement.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-export default function Advertisement(){
-    return (
-        <div>AD</div>
-    )
-}
-=======
 export default function Advertisement() {
   return <div></div>;
 }
->>>>>>> 9220d62f8915345240a956ff4bb5b0e242c5110d
